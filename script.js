e16fd1a67beb281520c973d6b97e3e3f64410c25
Scott Pribe <Scottpribe@gmail.com>
2020-03-28 10:29:09 -0400

    function in JS for default grid works. adjusted some of the button heights and added a random color function that will generate a random color.

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 const gridContainer = document.querySelector('#gridContainer');
 const divCreate = document.getElementsByClassName('divCreated');
 
-function defaultGrid (quantity=16) {
+function defaultGrid (quantity=16) { //default grid to 16x16 function
     for(i= 0; i < quantity ** 2; i++){
             const newDiv = document.createElement('div');
             newDiv.classList.add('divCreated');
             newDiv.id = i;
             gridContainer.appendChild(newDiv);
     }
-    document.documentElement.style.setProperty('--quantity',quantity)
+    document.documentElement.style.setProperty('--quantity',quantity);
 }
 
-document.getmyElementbyClassName('divCreated').innerHTML= defaultGrid(quantity=16);
+function randomColor () { // fills in random color once hovered over divs
+    hoverColor = Math.floor(Math.random()*16777215).toString(16);
+    document.getElementById('gridContainer').style.background = hoverColor;
+}
+
+function clearGrid () { // reset the grid to white 
+
+}
+
+function customGrid () {// prompts user to enter grid quantity between 2-64
+
+}
 
 
 
 
-
- 
+defaultGrid();
